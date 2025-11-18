79d6f15ddda4d871af9605501a04561de43d4840
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2025-11-17 20:07:11 -0800

    refactor: Rename lib.rs -> core.rs

---
+++ src/core.rs
@@ -0,0 +1,329 @@
+use nannou::prelude::*;
+use rand::Rng;
+
+// NB: Don't let it grow too big or you'll get stack overflows at compile time :)
+pub const GRID_SIZE: usize = 128;
+
+pub const GRID_LINE_WEIGHT: f32 = 0.3;
+pub const CELL_SIZE: usize = 4;
+
+// Data structures
+// ----------------------------------------------------------------------------
+#[derive(Debug, Copy, Clone)]
+pub struct CellIndex {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Cell {
+    pub is_alive: bool,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct CellsRow {
+    pub values: [Cell; GRID_SIZE],
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Cells {
+    pub rows: [CellsRow; GRID_SIZE],
+}
+
+pub enum AppState {
+    Init,
+    Running,
+}
+
+pub enum DrawingState {
+    Started,
+    Ended,
+    Void,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Line {
+    pub start_x: f32,
+    pub start_y: f32,
+    pub end_x: f32,
+    pub end_y: f32,
+    pub weight: f32,
+}
+
+pub struct Model {
+    pub lines: Vec<Line>,
+    pub cells: Cells,
+    pub cell_size: usize,
+    pub app_width: f32,
+    pub app_height: f32,
+    pub num_cells_x: usize,
+    pub num_cells_y: usize,
+    pub state: AppState,
+    pub should_draw_grid: bool,
+    pub should_draw_white: bool,
+    pub drawing_state: DrawingState,
+    pub current_stroke: Vec<Point2>,
+    pub grid_points: Vec<Point2>,
+    pub last_mouse_pos: Point2,
+    pub closest_points: Vec<Point2>,
+    pub generations: i64,
+}
+
+// Functions
+// ----------------------------------------------------------------------------
+pub fn get_neighbours_indices(x: usize, y: usize, cells: Cells) -> Vec<CellIndex> {
+    let mut neighbours = Vec::new();
+    let rows = cells.rows;
+
+    // Top neighbours
+    if y > 0 {
+        let top_index = y - 1;
+        let top_row = rows.get(top_index).unwrap();
+
+        for i in -1..2 {
+            let index = x as i32 + i;
+            let neighbour = top_row.values.get(index as usize);
+            if let Some(_x) = neighbour {
+                neighbours.push(CellIndex {
+                    x: index as usize,
+                    y: top_index,
+                })
+            }
+        }
+    }
+
+    // Bottom neighbours
+    if y < rows.len() - 2 {
+        let bottom_index = y + 1;
+        let bottom_row = rows.get(bottom_index).unwrap();
+
+        for i in -1..2 {
+            let index = x as i32 + i;
+            let neighbour = bottom_row.values.get(index as usize);
+            if let Some(_x) = neighbour {
+                neighbours.push(CellIndex {
+                    x: index as usize,
+                    y: bottom_index,
+                })
+            }
+        }
+    }
+
+    // Left and right neighbours
+    let c = rows.get(y);
+    if let Some(central_row) = c {
+        // Left
+        let l_index = x - 1;
+        let l_neighbour = central_row.values.get(l_index);
+        if let Some(_l) = l_neighbour {
+            neighbours.push(CellIndex { x: l_index, y })
+        }
+
+        // Right
+        let r_index = x + 1;
+        let r_neighbour = central_row.values.get(x + 1);
+        if let Some(_r) = r_neighbour {
+            neighbours.push(CellIndex { x: r_index, y })
+        }
+    }
+
+    neighbours
+}
+
+pub fn create_grid(app: &App, step_size: usize) -> Vec<Line> {
+    let mut lines = Vec::new();
+    let mut horizontal_lines = Vec::new();
+    let mut vertical_lines = Vec::new();
+
+    let window = app.window_rect();
+    let width = window.w() as i32;
+    let height = window.w() as i32;
+
+    let start_w = -width / 2;
+    let end_w = width / 2;
+
+    let start_h = -height / 2;
+    let end_h = -start_h;
+
+    // This is stored per-line so that one day this could procedural
+    // and different (eg: every N line, make a thicker one..)
+    let current_weight = GRID_LINE_WEIGHT;
+
+    // Horizontal lines
+    for i in (start_h..end_h).step_by(step_size) {
+        let current_y = i as f32;
+        //println!("Adding line at {}", current_y);
+
+        let line_props = Line {
+            start_x: start_w as f32,
+            end_x: end_w as f32,
+            start_y: current_y,
+            end_y: current_y,
+            weight: current_weight,
+        };
+
+        horizontal_lines.push(line_props);
+        lines.push(line_props);
+    }
+
+    // Vertical lines
+    for j in (start_w..end_w).step_by(step_size) {
+        let current_x = j as f32;
+
+        let line_props = Line {
+            start_x: current_x,
+            end_x: current_x,
+            start_y: start_h as f32,
+            end_y: end_h as f32,
+            weight: current_weight,
+        };
+
+        vertical_lines.push(line_props);
+        lines.push(line_props);
+    }
+
+    lines
+}
+
+pub fn draw_cell(x: usize, y: usize, alive: &bool, model: &Model, canvas: &Draw) {
+    let size = model.cell_size as f32;
+
+    // Convert from the 0,0 top-left system that I'm used to
+    // (because I programmed in Processing/Openframeworks for ages),
+    // to the 0,0-is-center system that nannou uses (which is the same
+    // that OpenGL uses, afaik)
+    let x_scaled = x as f32 * size;
+    let y_scaled = y as f32 * size;
+    let real_x = x_scaled - (model.app_width * 0.5) + (size * 0.5);
+    let real_y = -y_scaled + (model.app_height * 0.5) - (size * 0.5);
+
+    let color = if !alive { BLACK } else { RED };
+
+    canvas
+        .quad()
+        .w(size)
+        .h(size)
+        .x_y(real_x, real_y)
+        .color(color);
+}
+
+pub fn closest_n_points(in_point: Point2, points: &Vec<Point2>, n: usize) -> Vec<Point2> {
+    // Find the closest distance between the given point and
+    // all of the points in the grid
+    let mut distances = Vec::new();
+
+    for pt in points.iter() {
+        let dist = pt.distance(in_point);
+
+        distances.push((dist, pt));
+    }
+
+    // Sort by distance
+    distances.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    while distances.len() > n {
+        distances.pop();
+    }
+
+    let mut closest_points = Vec::new();
+    for (_dist, point) in distances {
+        closest_points.push(pt2(point.x, point.y));
+    }
+
+    assert_eq!(closest_points.len(), n);
+
+    closest_points
+}
+
+pub fn snap_to_grid(in_point: Point2, model: &Model) -> Point2 {
+    // Given a input point, find the closest point on the grid (by ceiling)
+    let closest_points = closest_n_points(in_point, &model.grid_points, 4);
+
+    let smallest_x: f32 = closest_points
+        .iter()
+        .map(|e| e.x)
+        .reduce(|accum, item| if accum < item { accum } else { item })
+        .unwrap();
+
+    let smallest_y: f32 = closest_points
+        .iter()
+        .map(|e| e.y)
+        .reduce(|accum, item| if accum < item { accum } else { item })
+        .unwrap();
+
+    pt2(smallest_x, smallest_y)
+}
+
+pub fn get_all_cells_as_dead() -> [CellsRow; GRID_SIZE] {
+    [CellsRow {
+        values: [Cell { is_alive: false }; GRID_SIZE],
+    }; GRID_SIZE]
+}
+
+pub fn init_cells(num_cells_x: usize, num_cells_y: usize, randomize: bool) -> Cells {
+    let mut generator = rand::thread_rng();
+    let mut rows = get_all_cells_as_dead();
+
+    for x in 0..num_cells_x {
+        let mut values = [Cell { is_alive: false }; GRID_SIZE];
+
+        for y in 0..num_cells_y {
+            if randomize {
+                values[y].is_alive = generator.gen_bool(0.5);
+            } else {
+                values[y].is_alive = false;
+            }
+        }
+        let row = CellsRow { values };
+
+        rows[x] = row;
+    }
+
+    Cells { rows }
+}
+
+pub fn game_of_life(model: &mut Model) {
+    // Shorthands
+    let cells = model.cells;
+    let rows = cells.rows;
+
+    for i in 0..model.num_cells_x {
+        let mut row = rows[i];
+
+        for j in 0..model.num_cells_y {
+            // Find neighbours
+            let neighbours_indices = get_neighbours_indices(i, j, cells);
+
+            let mut alive_neighbours = Vec::new();
+            let mut dead_neighbours = Vec::new();
+
+            for cell_index in neighbours_indices {
+                let cell = rows[cell_index.x].values[cell_index.y];
+                if cell.is_alive {
+                    alive_neighbours.push(cell);
+                } else {
+                    dead_neighbours.push(cell);
+                }
+            }
+
+            // Do the game of life..
+
+            // 1. Any live cell with two or three live neighbours survives
+            // 2. Any dead cell with three live neighbours becomes a live cell
+            // 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+            if row.values[j].is_alive {
+                match alive_neighbours.len() {
+                    2 | 3 => row.values[j] = Cell { is_alive: true },
+                    _ => row.values[j] = Cell { is_alive: false },
+                }
+            } else {
+                match alive_neighbours.len() {
+                    3 => row.values[j] = Cell { is_alive: true },
+                    _ => row.values[j] = Cell { is_alive: false },
+                }
+            }
+        }
+
+        model.cells.rows[i] = CellsRow { values: row.values };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod lib;
+mod core;
 
 use nannou::prelude::*;
 
@@ -6,14 +6,14 @@ fn main() {
     nannou::app(model).update(update).view(view).run();
 }
 
-fn key_pressed(_app: &App, model: &mut lib::Model, key: Key) {
+fn key_pressed(_app: &App, model: &mut core::Model, key: Key) {
     //println!("Key pressed: {:?}", key);
 
     match key {
         // Start
         Key::S => {
             println!("User pressed 'S' for 'Start'.");
-            model.state = lib::AppState::Running;
+            model.state = core::AppState::Running;
         }
         // Toggle grid
         Key::G => {
@@ -23,25 +23,25 @@ fn key_pressed(_app: &App, model: &mut lib::Model, key: Key) {
         // Clear
         Key::C => {
             println!("User pressed 'C' to clear the cells.");
-            model.cells.rows = lib::get_all_cells_as_dead();
+            model.cells.rows = core::get_all_cells_as_dead();
         }
         // Reset
         Key::R => {
             println!("User pressed 'R' for 'Reset'.");
 
             model.current_stroke = Vec::new();
-            model.state = lib::AppState::Init;
+            model.state = core::AppState::Init;
 
-            let cells = lib::init_cells(model.num_cells_x, model.num_cells_y, true);
+            let cells = core::init_cells(model.num_cells_x, model.num_cells_y, true);
             model.cells = cells;
         }
         _ => {}
     }
 }
 
-fn mouse_pressed(_app: &App, model: &mut lib::Model, button: MouseButton) {
+fn mouse_pressed(_app: &App, model: &mut core::Model, button: MouseButton) {
     println!("Mouse pressed: {button:?}");
-    model.drawing_state = lib::DrawingState::Started;
+    model.drawing_state = core::DrawingState::Started;
     match button {
         MouseButton::Left => model.should_draw_white = true,
         MouseButton::Right => model.should_draw_white = false,
@@ -49,17 +49,17 @@ fn mouse_pressed(_app: &App, model: &mut lib::Model, button: MouseButton) {
     }
 }
 
-fn mouse_moved(_app: &App, model: &mut lib::Model, pos: Point2) {
+fn mouse_moved(_app: &App, model: &mut core::Model, pos: Point2) {
     model.last_mouse_pos = pt2(pos.x, pos.y);
 
-    let closest_points = lib::closest_n_points(pos, &model.grid_points, 4);
+    let closest_points = core::closest_n_points(pos, &model.grid_points, 4);
     model.closest_points = closest_points;
 
-    if let lib::AppState::Init = model.state {
+    if let core::AppState::Init = model.state {
         // Start drawing
-        if let lib::DrawingState::Started = model.drawing_state {
+        if let core::DrawingState::Started = model.drawing_state {
             // Snap the point to the grid
-            let snapped = lib::snap_to_grid(pos, model);
+            let snapped = core::snap_to_grid(pos, model);
 
             // Discard clicks outside the target area
             if snapped.x.abs() > model.app_width * 0.5 {
@@ -91,16 +91,16 @@ fn mouse_moved(_app: &App, model: &mut lib::Model, pos: Point2) {
     }
 }
 
-fn mouse_released(_app: &App, model: &mut lib::Model, _button: MouseButton) {
+fn mouse_released(_app: &App, model: &mut core::Model, _button: MouseButton) {
     //
     //println!("Mouse released");
 
-    if let lib::DrawingState::Started = model.drawing_state {
-        model.drawing_state = lib::DrawingState::Ended;
+    if let core::DrawingState::Started = model.drawing_state {
+        model.drawing_state = core::DrawingState::Ended;
     }
 }
 
-fn model(app: &App) -> lib::Model {
+fn model(app: &App) -> core::Model {
     // Set up the window
     app.new_window()
         .title("Game of Life")
@@ -118,11 +118,11 @@ fn model(app: &App) -> lib::Model {
     let width = window_rect.w();
     let height = window_rect.h();
 
-    let num_cells_x = width as i32 / lib::CELL_SIZE as i32;
-    let num_cells_y = height as i32 / lib::CELL_SIZE as i32;
+    let num_cells_x = width as i32 / core::CELL_SIZE as i32;
+    let num_cells_y = height as i32 / core::CELL_SIZE as i32;
 
     // Initialize all of the cells
-    let cells = lib::init_cells(num_cells_x as usize, num_cells_y as usize, true);
+    let cells = core::init_cells(num_cells_x as usize, num_cells_y as usize, true);
 
     // Calculate the integers that make up the grid
     let w = (width) as i32;
@@ -130,17 +130,17 @@ fn model(app: &App) -> lib::Model {
 
     let mut grid_points = Vec::new();
 
-    for y in (-h..h).step_by(lib::CELL_SIZE) {
-        for x in (-w..w).step_by(lib::CELL_SIZE) {
+    for y in (-h..h).step_by(core::CELL_SIZE) {
+        for x in (-w..w).step_by(core::CELL_SIZE) {
             grid_points.push(pt2(x as f32, y as f32));
         }
     }
 
     // Create the lines that make up the grid
-    let lines = lib::create_grid(app, lib::CELL_SIZE);
+    let lines = core::create_grid(app, core::CELL_SIZE);
 
     println!("Canvas size is {width}x{height}");
-    println!("Cell size is {}", lib::CELL_SIZE);
+    println!("Cell size is {}", core::CELL_SIZE);
 
     println!("INSTRUCTIONS:");
     println!("Draw cells with the mouse left (alive) or right (dead) button");
@@ -149,18 +149,18 @@ fn model(app: &App) -> lib::Model {
     println!("Press 'R' to reset the simulation.");
     println!("Press 'C' to clear all cells (set all cells to dead).");
 
-    lib::Model {
+    core::Model {
         lines,
         cells,
-        cell_size: lib::CELL_SIZE,
+        cell_size: core::CELL_SIZE,
         app_width: width,
         app_height: height,
         num_cells_x: num_cells_x as usize,
         num_cells_y: num_cells_y as usize,
-        state: lib::AppState::Init,
+        state: core::AppState::Init,
         should_draw_grid: false,
         should_draw_white: true,
-        drawing_state: lib::DrawingState::Void,
+        drawing_state: core::DrawingState::Void,
         current_stroke: Vec::new(),
         grid_points,
         last_mouse_pos: pt2(0.0, 0.0),
@@ -169,20 +169,20 @@ fn model(app: &App) -> lib::Model {
     }
 }
 
-fn update(app: &App, model: &mut lib::Model, _update: Update) {
+fn update(app: &App, model: &mut core::Model, _update: Update) {
     if app.elapsed_frames() % 5 != 0 {
         return;
     }
 
     // Do the game of life only when needed
-    if let lib::AppState::Running = model.state {
-        lib::game_of_life(model);
+    if let core::AppState::Running = model.state {
+        core::game_of_life(model);
         model.generations += 1;
         println!("Generation: {}", model.generations);
     }
 }
 
-fn view(app: &App, model: &lib::Model, frame: Frame) {
+fn view(app: &App, model: &core::Model, frame: Frame) {
     let canvas = app.draw();
     canvas.background().color(BLACK);
 
@@ -193,7 +193,7 @@ fn view(app: &App, model: &lib::Model, frame: Frame) {
     // Draw the cells
     for (i, cell_row) in model.cells.rows.iter().enumerate() {
         for (j, cell_value) in cell_row.values.iter().enumerate() {
-            lib::draw_cell(i, j, &cell_value.is_alive, model, &canvas);
+            core::draw_cell(i, j, &cell_value.is_alive, model, &canvas);
         }
     }
 
